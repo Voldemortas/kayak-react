f5391483fa40262806adfa6436bed5397e328d49
Andrius Simanaitis <eragonas5@gmail.com>
2021-10-21 22:50:08 +0300

    [FEAT] added vehicles config json

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import logo from './logo.svg'
 import styles from './App.module.css'
 import { Template } from 'components'
 
--- src/components/template/Template.tsx
+++ src/components/template/Template.tsx
@@ -1,4 +1,3 @@
-import react from 'react'
 import styles from './Template.module.css'
 import useTemplate from './useTemplate'
 
