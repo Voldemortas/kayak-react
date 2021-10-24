3fabb5f12e337caeefc49e8e7ca9021cb499d5c6
Andrius Simanaitis <eragonas5@gmail.com>
2021-10-25 01:22:09 +0300

    [REFACT] added className to Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,17 +1,22 @@
 import styles from './Button.module.css'
-import { ReactNode } from 'react'
+import { MouseEvent, ReactNode } from 'react'
 import { Tabbable } from 'components'
 
 export interface Props {
-  onClick: () => void
+  onClick: (event: MouseEvent) => void
   children: ReactNode
   selected?: boolean
+  className?: string
 }
 
 const VehicleButton = (props: Props) => {
-  const { onClick, children, selected } = props
+  const { onClick, children, selected, className } = props
 
-  const combinedStyle = [styles.container, !!selected ? styles.selected : null]
+  const combinedStyle = [
+    className,
+    styles.container,
+    !!selected ? styles.selected : null,
+  ]
 
   return (
     <Tabbable onClick={onClick} className={combinedStyle.join(' ')}>
